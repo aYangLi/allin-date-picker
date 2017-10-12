56e5a296298f676e075e2f51b8b6d13b74df8671
lichenyang <lichenyang@allinmd.cn>
2017-10-12 19:24:27 +0800

    添加使用文档

--- src/picker/pickerDate.js
+++ src/picker/pickerDate.js
@@ -1,3 +1,33 @@
+/**
+ * @Desc：基于better-picker的时间选择器；
+ * @Usage:
+ *          需要初始化选择器var picker = new PickerDate(options);
+ *          var options = {
+ *            startYear:1990,   开始时间
+              lastYear:2020,    结束时间，不传默认为当前年份
+              title: '日期选择',    选择器标题
+              reset：true，//是否超出当前日期回滚到当前日期；
+              changeFun:function (index,val) {
+                  index（改变的是那一列；0-年；1-月；2-日）
+                  val（改变那一列的索引值）
+              },//当一列滚动停止的时候
+              selectFun:function (index,val) {
+                  index（选择的索引数组）
+                  val（选择的值的数组）
+              }，//当用户点击确定的时候，
+              valueChangeFun: function (index,val){
+                  index（选择的索引数组）
+                  val（选择的值的数组）
+              }，当用户点击确定的时候，如果本次选择的数据和上一次不一致，
+              cancelFun:function () {
+                当用户点击取消的时候触发
+              }
+ *          }
+ * @Notify：
+ * @Depend：better-picker.js
+ *
+ * Created by lichenyang on 2017/10/12.
+ */
 import Picker from './picker';
 import getDate from '../util/getDate';
 
@@ -33,11 +63,16 @@ export default class PickerDate {
     });
     this.datePicker.on('picker.select', (selectedIndex, selectedVal) => {
       let returnArr = selectedVal.toString().split(',');
+      let valArr = [yearArr[returnArr[0]], monthArr[returnArr[1]], dayArr[returnArr[2]]];
+      this.options.selectFun && this.options.selectFun(selectedIndex, valArr);
+    });
+    this.datePicker.on('picker.valuechange', (selectedIndex, selectedVal) => {
+      let returnArr = selectedVal.toString().split(',');
 //          console.log(yearData[returnArr[0]].value);
 //          console.log(monthData[returnArr[1]]);
 //          console.log(dayData[returnArr[2]]);
       let valArr = [yearArr[returnArr[0]], monthArr[returnArr[1]], dayArr[returnArr[2]]];
-      this.options.selectFun && this.options.selectFun(selectedIndex, valArr);
+      this.options.valueChangeFun && this.options.valueChangeFun(selectedIndex, valArr);
     });
     this.datePicker.on('picker.change', (index, selectedIndex) => {
       console.log(index);
